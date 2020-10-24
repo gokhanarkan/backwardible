b87361f22957402d1cb335f4c39856dd039aebd7
Gokhan <gokhanarkan@gmail.com>
2020-10-24 14:13:19 +0300

    getting an initial draft on pagination and content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @games = Game.all
+    page = params[:id]
+    @games = Game.page(page).per(12)
+    @total_pages = @games.total_pages
+    @previous_page = @games.prev_page
+    @next_page = @games.next_page
+    @first_page = @games.first_page?
+    @last_page = @games.last_page?
+    @current_page = @games.current_page
+    @total_pages = @games.total_pages
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 require 'sidekiq/web'
 
+# Rails.application.routes.draw do
+#     authenticate :user, lambda { |u| u.admin? } do
+#       mount Sidekiq::Web => '/sidekiq'
+#     end
+
+
+#   devise_for :users
+#   root to: 'home#index'
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
 
+  root 'home#index'
+  get ':id', to: 'home#index'
 
-  devise_for :users
-  root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
