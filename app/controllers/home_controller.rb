eea2ac5b2a71bbcc72f8c1621a480dce43490dec
Gokhan <gokhanarkan@gmail.com>
2020-10-24 16:04:04 +0300

    Pagination completed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    page = params[:id]
+    page = params[:page]
     @games = Game.page(page).per(12)
     @total_pages = @games.total_pages
     @previous_page = @games.prev_page
@@ -9,5 +9,13 @@ class HomeController < ApplicationController
     @last_page = @games.last_page?
     @current_page = @games.current_page
     @total_pages = @games.total_pages
+    @page_range = 1..3
+    if @first_page
+      @page_range = 1..3
+    elsif @last_page
+      @page_range = @current_page-1..@current_page
+    else
+      @page_range = @current_page-1..@current_page+1
+    end
   end
 end
